101ce3ff8acf493d67b4d83c7a5e142f5abd1a8f
sophia-jihye <jihyeparkk@naver.com>
2019-10-30 15:31:58 +0900

    top5 based on frequency, uniqueness, strength. csv

---
+++ source/create_top5_csv_before_visualization.py
@@ -0,0 +1,37 @@
+from config import parameters
+from utils import *
+import os
+
+# User configuration
+max_display_count = 5
+
+output_base_dir = parameters.output_base_dir
+top5_csv_filepath = os.path.join(output_base_dir, get_str_concat('top5-frequency-uniqueness-strength',
+                                                                 get_now_time_str()) + '.csv')
+bigram_uniqueness_strength_dict_filepath = '/home/dmlab/jihye/GIT/bis_speeches_text_bigram_frequency/output/bigram-uniqueness-strength.pkl'
+bigram_uniqueness_strength_dict = load_pkl(bigram_uniqueness_strength_dict_filepath)
+
+printing_order_dict = {0: 'frequency', 1: 'uniqueness', 2: 'strength'}
+
+
+def main():
+    f, wr = start_csv(top5_csv_filepath, csv_delimiter=',')
+    for printing_order in printing_order_dict.keys():
+        for _period_category, _period_dict in bigram_uniqueness_strength_dict.items():
+            wr.writerow([printing_order_dict[printing_order]])
+            wr.writerow([_period_category, 'bigram', 'frequency', 'uniqueness', 'strength'])
+            for _period in sorted(_period_dict.keys()):
+                count = 0
+                for _bigram, (_freq, _uniqueness, _strength) in sorted(_period_dict[_period].items(),
+                                                                       key=lambda t: t[-1][printing_order],
+                                                                       reverse=True):  # CONTROL PRINTING ORDER
+                    count += 1
+                    wr.writerow([_period, _bigram, _freq, _uniqueness, _strength])
+                    if count == max_display_count:
+                        break
+
+    end_csv(f, top5_csv_filepath)
+
+
+if __name__ == '__main__':
+    main()
