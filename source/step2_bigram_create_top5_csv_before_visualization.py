ff78aac649597097f1a036320ebfde66a3802399
sophia-jihye <jihyeparkk@naver.com>
2019-11-01 12:16:50 +0900

    applying lookup table results to csv

--- source/step2_bigram_create_top5_csv_before_visualization.py
+++ source/step2_bigram_create_top5_csv_before_visualization.py
@@ -6,10 +6,11 @@ import os
 max_display_count = 5
 
 output_base_dir = parameters.output_base_dir
+bigram_uniqueness_strength_dict_filepath = 'bigram-uniqueness-strength_20191101-02-55-52.pkl'
+bigram_uniqueness_strength_dict = load_pkl(os.path.join(output_base_dir, bigram_uniqueness_strength_dict_filepath))
+
 top5_csv_filepath = os.path.join(output_base_dir, get_str_concat('top5-frequency-uniqueness-strength',
                                                                  get_now_time_str()) + '.csv')
-bigram_uniqueness_strength_dict_filepath = '/home/dmlab/jihye/GIT/bis_speeches_text_bigram_frequency/output/bigram-uniqueness-strength.pkl'
-bigram_uniqueness_strength_dict = load_pkl(bigram_uniqueness_strength_dict_filepath)
 
 printing_order_dict = {0: 'frequency', 1: 'uniqueness', 2: 'strength'}
 
@@ -18,15 +19,14 @@ def main():
     f, wr = start_csv(top5_csv_filepath, csv_delimiter=',')
     for printing_order in printing_order_dict.keys():
         for _period_category, _period_dict in bigram_uniqueness_strength_dict.items():
-            wr.writerow([printing_order_dict[printing_order]])
-            wr.writerow([_period_category, 'bigram', 'frequency', 'uniqueness', 'strength'])
+            wr.writerow(['top5_criteria' 'period_category', 'period', 'bigram', 'frequency', 'uniqueness', 'strength'])
             for _period in sorted(_period_dict.keys()):
                 count = 0
                 for _bigram, (_freq, _uniqueness, _strength) in sorted(_period_dict[_period].items(),
                                                                        key=lambda t: t[-1][printing_order],
                                                                        reverse=True):  # CONTROL PRINTING ORDER
                     count += 1
-                    wr.writerow([_period, _bigram, _freq, _uniqueness, _strength])
+                    wr.writerow([printing_order_dict[printing_order], _period_category, _period, _bigram, _freq, _uniqueness, _strength])
                     if count == max_display_count:
                         break
 
